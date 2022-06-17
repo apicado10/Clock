e82597c23b5f2631c82eef869c9bcac7e16078d5
Ana Picado <anapicado@Anas-MacBook-Air.local>
2022-06-16 21:20:02 -0600

    taller 7 - clock

---
+++ src/components/Controls.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Clock from './Clock';
+
+class Controls extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            active: false,
+        };
+    }
+
+    render() {
+        return(
+            <>
+            <button onClick={ ()=> this.setState({ active: !this.state.active}) }>
+                {this.state.active ? "On" : "Off"} 
+            </button>
+            { this.state.active ?
+            <Clock /> :
+            <p>Clock off</p>
+            }
+            </>
+        );
+    }
+}
+
+export default Controls;
